0f5d7c6086521407e727de51a2ca8b7dd880708d
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-01-13 18:22:06 +0400

    Add script: build-clang.sh

---
+++ llvm/build-clang.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+set -x
+
+cd "`dirname "${BASH_SOURCE[0]}"`"
+cd ..
+
+GITENV_ROOT="`pwd`"
+
+# http://clang.llvm.org/get_started.html
+
+for curr in llvm/llvm llvm/clang llvm/clang-tools-extra llvm/compiler-rt;
+do
+  cd "${GITENV_ROOT}"
+  git submodule update --init $curr
+  cd $curr
+  git checkout master
+  git pull
+done
+
+rm -f "${GITENV_ROOT}/llvm/llvm/tools/clang/tools/clang-tools-extra"
+rm -f "${GITENV_ROOT}/llvm/llvm/tools/clang"
+rm -f "${GITENV_ROOT}/llvm/llvm/projects/compiler-rt"
+
+ln -s "${GITENV_ROOT}/llvm/clang" "${GITENV_ROOT}/llvm/llvm/tools/clang"
+
+ln -s "${GITENV_ROOT}/llvm/clang-tools-extra" \
+    "${GITENV_ROOT}/llvm/llvm/tools/clang/tools/clang-tools-extra"
+
+ln -s "${GITENV_ROOT}/llvm/compiler-rt" \
+     "${GITENV_ROOT}/llvm/llvm/projects/compiler-rt"
+
+cd "${GITENV_ROOT}/llvm"
+
+rm -rf _builds
+
+cmake -Hllvm -B_builds -DCMAKE_VERBOSE_MAKEFILE=ON \
+    -DCMAKE_INSTALL_PREFIX="`pwd`/_install" -DCMAKE_BUILD_TYPE=Release
+
+cmake --build _builds --target install
