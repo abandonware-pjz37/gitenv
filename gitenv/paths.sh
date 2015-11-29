c013ba774d56b3040d43f7c0997f6382506fd231
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-11-29 14:30:33 +0600

    bash compatible comparison operator

--- gitenv/paths.sh
+++ gitenv/paths.sh
@@ -15,50 +15,50 @@ if [ -n "${GITENV_ROOT}" ];
 then
   if [ -r "${GITENV_ROOT}/git/contrib/completion/git-completion.bash" ];
   then
-    [ "${GITENV_VERBOSE}" == "1" ] && echo "[gitenv] add git-completion.bash"
+    [ "${GITENV_VERBOSE}" = "1" ] && echo "[gitenv] add git-completion.bash"
     source "${GITENV_ROOT}/git/contrib/completion/git-completion.bash"
   fi
 
   if [ -r "${GITENV_ROOT}/ruslo/sugar/cmake/Sugar" ];
   then
     export SUGAR_ROOT="${GITENV_ROOT}/ruslo/sugar"
-    [ "${GITENV_VERBOSE}" == "1" ] && echo "[gitenv] set SUGAR_ROOT to '${SUGAR_ROOT}'"
+    [ "${GITENV_VERBOSE}" = "1" ] && echo "[gitenv] set SUGAR_ROOT to '${SUGAR_ROOT}'"
     if [ -r "${SUGAR_ROOT}/python" ];
     then
-      [ "${GITENV_VERBOSE}" == "1" ] && echo "[gitenv] add '${SUGAR_ROOT}/python' directory to PATH"
+      [ "${GITENV_VERBOSE}" = "1" ] && echo "[gitenv] add '${SUGAR_ROOT}/python' directory to PATH"
       export PATH="${SUGAR_ROOT}/python":${PATH}
     fi
   fi
 
   if [ -r "${GITENV_ROOT}/ruslo/polly/utilities/polly_common.cmake" ];
   then
-    [ "${GITENV_VERBOSE}" == "1" ] && echo "[gitenv] add '${GITENV_ROOT}/ruslo/polly/bin' directory to PATH"
+    [ "${GITENV_VERBOSE}" = "1" ] && echo "[gitenv] add '${GITENV_ROOT}/ruslo/polly/bin' directory to PATH"
     export PATH="${GITENV_ROOT}/ruslo/polly/bin":${PATH}
   fi
 
   if [ -r "${GITENV_ROOT}/ruslo/hunter/cmake/Hunter" ];
   then
     export HUNTER_ROOT="${GITENV_ROOT}/ruslo/hunter"
-    [ "${GITENV_VERBOSE}" == "1" ] && echo "[gitenv] set HUNTER_ROOT to '${HUNTER_ROOT}'"
+    [ "${GITENV_VERBOSE}" = "1" ] && echo "[gitenv] set HUNTER_ROOT to '${HUNTER_ROOT}'"
   fi
 
   if [ -r "${GITENV_ROOT}/llvm/_install/bin/clang" ];
   then
-    [ "${GITENV_VERBOSE}" == "1" ] && echo "[gitenv] add '${GITENV_ROOT}/llvm/_install/bin' directory to PATH"
+    [ "${GITENV_VERBOSE}" = "1" ] && echo "[gitenv] add '${GITENV_ROOT}/llvm/_install/bin' directory to PATH"
     export PATH="${GITENV_ROOT}/llvm/_install/bin":${PATH}
   fi
 
   asan="${GITENV_ROOT}/llvm/compiler-rt/lib/asan/scripts"
   if [ -r "${asan}/asan_symbolize.py" ];
   then
-    [ "${GITENV_VERBOSE}" == "1" ] && echo "[gitenv] add '${asan}' directory to PATH"
+    [ "${GITENV_VERBOSE}" = "1" ] && echo "[gitenv] add '${asan}' directory to PATH"
     export PATH="${asan}":${PATH}
   fi
 
   cmake_path="${GITENV_ROOT}/cmake/_install/default/bin"
   if [ -r "${cmake_path}/cmake" ];
   then
-    [ "${GITENV_VERBOSE}" == "1" ] && echo "[gitenv] add '${cmake_path}' directory to PATH"
+    [ "${GITENV_VERBOSE}" = "1" ] && echo "[gitenv] add '${cmake_path}' directory to PATH"
     export PATH="${cmake_path}":${PATH}
   fi
 fi
